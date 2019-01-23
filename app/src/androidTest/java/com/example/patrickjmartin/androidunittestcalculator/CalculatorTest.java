2f43621ef74547d3d44ee5273017d54ec5fe4006
Patrick Martin <patrickmartin88@gmail.com>
2019-01-23 15:54:34 -0800

    Initial Calculator class without using stack.

--- app/src/androidTest/java/com/example/patrickjmartin/androidunittestcalculator/CalculatorTest.java
+++ app/src/androidTest/java/com/example/patrickjmartin/androidunittestcalculator/CalculatorTest.java
@@ -1,26 +1,48 @@
 package com.example.patrickjmartin.androidunittestcalculator;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 
 public class CalculatorTest {
 
-    @Test
-    public void addSymbolreturnsSameString() {
-        assertEquals("+", Calculator.addSymbol("+"));
+    Calculator calculator;
+
+    @Before
+    public void setUp() throws Exception {
+        calculator = new Calculator();
     }
 
-    @Test
-    public void addSymbolshouldReturnMultipleStringsWhenMoreSymbolsAdded() {
-        String multipleStringsCombined = "5.92";
+    
 
-        Calculator.addSymbol("5");
-        Calculator.addSymbol(".");
-        Calculator.addSymbol("9");
 
-        assertEquals(multipleStringsCombined, Calculator.addSymbol("2"));
-    }
+//    Old Test
+//    @Test
+//    public void addSymbolreturnsSameStringInitially() {
+//        //Setup
+//        String expected = "+";
+//
+//        //Execution
+//        String actual = calculator.addSymbol("+");
+//
+//        //Check
+//        //Success
+//        assertEquals("+", calculator.addSymbol("+"));
+//    }
+
+//    Old Test
+//    @Test
+//    public void addSymbolshouldReturnMultipleStringsWhenMoreSymbolsAdded() {
+//        String multipleStringsCombined = "5.92";
+//
+//        Calculator.addSymbol("5");
+//        Calculator.addSymbol(".");
+//        Calculator.addSymbol("9");
+//
+//        //Failure
+//        assertEquals(multipleStringsCombined, calculator.addSymbol("2"));
+//    }
 
 
 }
--- app/src/main/java/com/example/patrickjmartin/androidunittestcalculator/Calculator.java
+++ app/src/main/java/com/example/patrickjmartin/androidunittestcalculator/Calculator.java
@@ -1,13 +1,103 @@
 package com.example.patrickjmartin.androidunittestcalculator;
 
+import android.support.v4.view.PagerTabStrip;
+
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
+import java.util.Stack;
+
 public class Calculator {
 
+//    private String currentTotal;
+//    private String finalTotal;
+    private Stack<Double> numberStack;
+    private Stack<String> operatorStack;
+    NumberFormat stringFormat = new DecimalFormat("#.####");
+
+
+
     public Calculator() {
+        numberStack = new Stack<>();
+        operatorStack = new Stack<>();
     }
 
-    public static String addSymbol(String symbol) {
-
-
-        return symbol;
+    public void numberStack(String inputNumber) {
+        double number = Double.parseDouble(inputNumber);
+        numberStack.push(number);
     }
+
+    public void operationStack(String operator) {
+        operatorStack.push(operator);
+    }
+
+    public String addOperation(String input) {
+        String result = "";
+        //TODO
+        return result;
+    }
+
+    public String addOperation(String input1, String input2) {
+        Double in1Double = Double.parseDouble(input1);
+        Double in2Double = Double.parseDouble(input2);
+
+        Double doubleResult = in1Double + in2Double;
+
+        return stringFormat.format(doubleResult);
+    }
+
+    public String subtractOperation(String input) {
+        String result = "";
+        //TODO
+
+        return result;
+    }
+
+    public String subtractOperation(String input1, String input2) {
+
+
+        Double in1Double = Double.parseDouble(input1);
+        Double in2Double = Double.parseDouble(input2);
+
+        Double doubleResult = in1Double - in2Double;
+
+        return stringFormat.format(doubleResult);
+    }
+
+    public String multiplyOperation(String input) {
+        String result = "";
+        //TODO
+        return result;
+    }
+
+    public String multiplyOperation(String input1, String input2) {
+        Double in1Double = Double.parseDouble(input1);
+        Double in2Double = Double.parseDouble(input2);
+
+        Double doubleResult = in1Double * in2Double;
+
+        return stringFormat.format(doubleResult);
+    }
+
+    public String divideOperation(String input) {
+        String result = "";
+        //TODO
+        return result;
+    }
+
+    public String divideOperation(String input1, String input2) {
+        Double in1Double = Double.parseDouble(input1);
+        Double in2Double = Double.parseDouble(input2);
+
+        if(in2Double == 0) {
+            return "DNE";
+        } else {
+            Double doubleResult = in1Double * in2Double;
+            return stringFormat.format(doubleResult);
+        }
+    }
+
+
+
+
+
 }
