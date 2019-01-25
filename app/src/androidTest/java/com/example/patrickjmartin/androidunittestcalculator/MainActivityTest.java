adebafcd0443b22b54fcc97224381bfb67e16d66
Patrick Martin <patrickmartin88@gmail.com>
2019-01-24 16:42:55 -0800

    Creating rule.

--- app/src/androidTest/java/com/example/patrickjmartin/androidunittestcalculator/MainActivityTest.java
+++ app/src/androidTest/java/com/example/patrickjmartin/androidunittestcalculator/MainActivityTest.java
@@ -1,12 +1,15 @@
 package com.example.patrickjmartin.androidunittestcalculator;
 
 import org.junit.Before;
+import org.junit.Rule;
+
+import androidx.test.rule.ActivityTestRule;
 
 import static org.junit.Assert.*;
 
 public class MainActivityTest {
 
-    @Before
-    public void setUp() throws Exception {
-    }
+    @Rule
+    public ActivityTestRule<MainActivity> activityTestRule = new ActivityTestRule<>
+            (MainActivity.class);
 }
